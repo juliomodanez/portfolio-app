458007888bf08b304f6fff405a6fbe66552fd99c
matheusmodanez <matheus.modanez@gmail.com>
2023-04-08 23:02:47 -0300

    .

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -1,7 +1,23 @@
 import React, { useState } from 'react'
-import { ImageList, ImageListItem } from '@mui/material'
+import { Box, ImageList, ImageListItem, Modal, Typography } from '@mui/material'
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 function Category({ allContent }) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <div>
             <ImageList
@@ -17,9 +33,24 @@ function Category({ allContent }) {
                                 src={item.source}
                                 alt={item.title}
                                 loading="lazy"
+
+                                onClick={handleOpen}
                             />
 
                         </ImageListItem>
+
+                        <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box sx={style}>
+                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                    {item.title}
+                                </Typography>
+                            </Box>
+                        </Modal>
                     </div>
                 ))}
             </ImageList>
