9a04274067204178cca957fd9b50dded26eb1819
juliomodanez <julio.modanez@gmail.com>
2023-06-28 18:36:55 -0300

    content_HL edited

--- src/contents/Content_HL.js
+++ src/contents/Content_HL.js
@@ -1,7 +1,28 @@
-const highlights = [{
-    title: "Drown",
+import Anvil from "../images/anvil.png"
+import Coast04_01 from "../images/coast_04/IMG_1841.jpg"
+import Donutimg from "../images/donut/Donut.png"
+import Telhados from "../images/telhados_001.png"
+
+const highlights = [
+  {
+    title: "Bigorna",
+    tags: "3D",
+    source: Anvil,
+  },
+  {
+    title: "Encosta 04",
     tags: "gravura",
-    source: Drown,
+    source: Coast04_01,
+  },
+  {
+    title: "Donut",
+    tags: "3D",
+    source: Donutimg,
+  },
+  {
+    title: "Telhados",
+    tags: "3D",
+    source: Telhados,
   },
 
 ]
